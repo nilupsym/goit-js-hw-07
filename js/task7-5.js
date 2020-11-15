99149729fbaba95c51aaf8bfb264077fbf6317e8
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2020-11-16 00:55:41 +0200

    Update task7-5.js

--- js/task7-5.js
+++ js/task7-5.js
@@ -4,4 +4,4 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 
-nameInput.addEventListener('input', event => {if (event.target.value === '') {nameOutput.textContent = 'незнакомец';} nameOutput.textContent = event.target.value;});
+nameInput.addEventListener('input', event => {if (event.target.value === '') {nameOutput.textContent = 'незнакомец';} else {nameOutput.textContent = event.target.value;}});
