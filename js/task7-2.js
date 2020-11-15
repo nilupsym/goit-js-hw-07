09b6bc86e3c3de70eaf99c04fd77dc1ec0cfaa18
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2020-11-15 13:25:33 +0200

    Update task7-2.js

--- js/task7-2.js
+++ js/task7-2.js
@@ -15,26 +15,37 @@ const ingredients = [
 
 const ul = document.querySelector('#ingredients');
 
-const li1 = document.createElement('li');
-li1.textContent = ingredients[0];
+// const li1 = document.createElement('li');
+// li1.textContent = ingredients[0];
 
-const li2 = document.createElement('li');
-li2.textContent = ingredients[1];
+// const li2 = document.createElement('li');
+// li2.textContent = ingredients[1];
 
-const li3 = document.createElement('li');
-li3.textContent = ingredients[2];
+// const li3 = document.createElement('li');
+// li3.textContent = ingredients[2];
 
-const li4 = document.createElement('li');
-li4.textContent = ingredients[3];
+// const li4 = document.createElement('li');
+// li4.textContent = ingredients[3];
 
-const li5 = document.createElement('li');
-li5.textContent = ingredients[4];
+// const li5 = document.createElement('li');
+// li5.textContent = ingredients[4];
 
-const li6 = document.createElement('li');
-li6.textContent = ingredients[5];
+// const li6 = document.createElement('li');
+// li6.textContent = ingredients[5];
 
-const lis = [li1, li2, li3, li4, li5, li6];
+// const lis = [li1, li2, li3, li4, li5, li6];
 
-ul.append(...lis);
+// ul.append(...lis);
 
+// console.log(ul);
+
+const ingredientsItem = ingredient => {
+    const li = document.createElement('li');
+    li.textContent = ingredient;
+    return li;
+}
+
+const addIngredient = ingredients.map(ingredient => ingredientsItem(ingredient));
+
+ul.append(...addIngredient);
 console.log(ul);
